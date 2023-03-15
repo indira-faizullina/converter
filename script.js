d626995626b2262e25361f95055b43fd5dc06393
indira-faizullina <indiramus@list.ru>
2023-03-15 23:29:21 +1100

    add Kelvin

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const inputFirst = document.querySelector('#first-value')
 const inputSecond = document.querySelector('#second-value')
+const inputThird = document.querySelector('#third-value')
 
 // (Фаренгейт — 32) : 1,8 = Цельсий
 // Цельсий х 1,8 + 32 = Фаренгейт
@@ -7,15 +8,35 @@ const inputSecond = document.querySelector('#second-value')
 inputFirst.addEventListener('input', (event) => {
   if (!event.target.value) {
     inputSecond.value = ''
+    inputThird.value = ''
   } else {
-    inputSecond.value = (event.target.value - 32) / 1.8
+    inputSecond.value = ((Number(event.target.value) - 32) / 1.8).toFixed(3)
+    inputThird.value = (
+      ((Number(event.target.value) - 32) * 5) / 9 +
+      273.15
+    ).toFixed(3)
   }
 })
 
 inputSecond.addEventListener('input', (event) => {
   if (!event.target.value) {
     inputFirst.value = ''
+    inputThird.value = ''
   } else {
-    inputFirst.value = event.target.value * 1.8 + 32
+    inputFirst.value = (Number(event.target.value) * 1.8 + 32).toFixed(3)
+    inputThird.value = (Number(event.target.value) + 273.15).toFixed(3)
+  }
+})
+
+inputThird.addEventListener('input', (event) => {
+  if (!event.target.value) {
+    inputFirst.value = ''
+    inputSecond.value = ''
+  } else {
+    inputFirst.value = (
+      (Number(event.target.value - 273.15) * 9) / 5 +
+      32
+    ).toFixed(3)
+    inputSecond.value = (Number(event.target.value) - 273.15).toFixed(3)
   }
 })
